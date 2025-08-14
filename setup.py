d5ebc529fb2d3e058ee9bf30a240ee2efe13e6c5
andrewfulton9 <andrewfulton9@gmail.com>
2025-08-14 19:23:20 +0000

    decrease memory usage

--- setup.py
+++ setup.py
@@ -110,6 +110,9 @@ class _BazelBuildCommand(setuptools.Command):
          '--compilation_mode', 'opt',
          '--define', 'grpc_no_ares=true',
          '--verbose_failures',
+         '--discard_analysis_cache',
+         '--worker_max_instances=1',
+         '--notrack_incremental_state',
          *self._additional_build_options,
          '//ml_metadata:move_generated_files'],
         # Bazel should be invoked in a directory containing bazel WORKSPACE
